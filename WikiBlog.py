0dcc3739aa1311dff89e8ceac75cc3079336e4a3
Hasitha <renukahasitha6@gmail.com>
2025-12-23 10:09:02 +0530

    WikiBlog Code with comments

--- WikiBlog.py
+++ WikiBlog.py
@@ -1,7 +1,7 @@
 import wikipedia     #run this in virtual env:  source ~/myenv/bin/activate
 import sys 
 import time 
-from datetime import datetime 
+from datetime import datetime  #Timely greetings
 
 def greet():
     print("Welcome back to WikiBlog!")
@@ -9,7 +9,7 @@ def greet():
     time1 = datetime.now().hour
 
     if 5 <= time1 <= 11:
-        print(f"Good Morning {name.capitalize()}! What do you want to talk about today?")
+        print(f"Good Morning {name.capitalize()}! What do you want to talk about today?")   #name.capitalize() will capitalize the name
     elif 12 <= time1 <= 15:
         print(f"Good Afternoon {name.capitalize()}! What's new?")
     elif 15 <= time1 <= 19:
@@ -17,7 +17,7 @@ def greet():
     else:
         print(f"Hello {name.capitalize()}! What's up?")
 
-def prompting():
+def prompting():      #this function is for the message that you give
     while True:
         prompt = input("Enter your message: ")
 
@@ -26,13 +26,13 @@ def prompting():
             sys.exit()
 
         try:
-            topics = wikipedia.search(prompt)
-            res = topics[0]
+            topics = wikipedia.search(prompt)        #The wikipedia.search(prompt) will return a list of topics
+            res = topics[0]        #We will choose the first list element to generate a para
 
             print("\n Generating your prompt.This may Take a while...")
 
             if "detailed" in prompt.lower() or "detail" in prompt.lower():
-                page = wikipedia.page(res)
+                page = wikipedia.page(res)    #wikipedia.page() will return the entire page 
                 content = page.content
                 print(f"\n Here is what I found about {prompt}")
                 words1 = content.split()
@@ -43,18 +43,18 @@ def prompting():
 
             try:
                 for word in words1:
-                    print(word, end=' ', flush=True)
+                    print(word, end=' ', flush=True)   #flush=True is used to generate output with a delay of 0.1sec
                     time.sleep(0.1)
                 print()
-            except KeyboardInterrupt:
-                print("\nInterrupted. You can enter a new message.")
+            except KeyboardInterrupt:        #Even if there's any keyboard interrupt ctrl+c the program shouldn't stop
+                print("\nInterrupted. You can enter a new message.") 
                 continue
 
-        except wikipedia.exceptions.PageError:
+        except wikipedia.exceptions.PageError:     #Even if there is no content in the wikipedia page it should still continue
             print("Couldn't find summary on that topic")
             continue
 
-        except wikipedia.exceptions.DisambiguationError as e:
+        except wikipedia.exceptions.DisambiguationError as e:   #if you didnt give the prompt right
             print("Give some other input. The input is not valid.")
             print(e.options)
             continue
@@ -62,7 +62,7 @@ def prompting():
         time.sleep(2)
 
         # ================= TOPIC SECTION =================
-        while True:
+        while True:       #after generating list[0] we will be showing list of other topics to view
             j = 0
             print("\nDo you want me to generate para to the related topics?")
             time.sleep(2)
